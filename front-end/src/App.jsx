4621786e56188de23aca5df29a6897bf444492d1
AhmedSamehGad <AhmedSamehGad>
2025-10-01 17:21:45 +0300

    main-routes-init

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { Route , Routes , BrowserRouter as Router } from 'react-router-dom'
+import Home from './Pages/Home';
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import UserDashboard from "./Pages/UserDashboard";
+import AdminDashboard from "./Pages/AdminDashboard"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1 className='text-blue-600'>Welcom to tailwind setup</h1>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/user-dashboard' element={<UserDashboard />} />
+        <Route path='/admin-dashboard' element={<AdminDashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
